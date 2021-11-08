59034dc7336a58e850f6cefcbf1caa0389740189
morteza shojaei <mshojaei179@gmail.com>
2021-11-08 11:06:40 +0330

    fix typo

--- 4/Q1/code/HelloWorld.c
+++ 4/Q1/code/HelloWorld.c
@@ -1,26 +1,3 @@
-/*******************************************************
-This program was created by the
-CodeWizardAVR V3.12 Advanced
-Automatic Program Generator
-© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
-http://www.hpinfotech.com
-
-Project : LCDHelloWorld
-Version : 
-Date    : 12/28/2020
-Author  : 
-Company : 
-Comments: 
-
-
-Chip type               : ATmega16
-Program type            : Application
-AVR Core Clock frequency: 12.000000 MHz
-Memory model            : Small
-External RAM size       : 0
-Data Stack size         : 256
-*******************************************************/
-
 #include <mega16.h>
 #include <alcd.h>
 
